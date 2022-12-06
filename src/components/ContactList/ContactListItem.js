4c11f5e7d696cca2dfc03c3963178603ec071c9a
Anton <a.chertok@akatech.com.ua>
2022-12-06 12:26:12 +0200

    todo

--- src/components/ContactList/ContactListItem.js
+++ src/components/ContactList/ContactListItem.js
@@ -1,13 +1,14 @@
 import Button from 'components/Button';
+import PropTypes from 'prop-types';
 import { ListItem, Name, Number, ContactWrapper } from './ContactList.styled';
 
 const ContactListItem = ({
   name,
   phone,
   onDelete,
+  onEdit,
   isDeleting,
   isUpdating,
-  onEdit,
 }) => {
   return (
     <ListItem>
@@ -15,16 +16,25 @@ const ContactListItem = ({
         <Name>{name}:</Name>
         <Number>{phone}</Number>
 
-        <Button type="button" onClick={onDelete} disabled={isDeleting}>
-          Delete
-        </Button>
-
         <Button type="button" onClick={onEdit} disabled={isUpdating}>
           Edit
+        </Button>
+
+        <Button type="button" onClick={onDelete} disabled={isDeleting}>
+          Delete
         </Button>
       </ContactWrapper>
     </ListItem>
   );
+};
+
+ContactListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  isDeleting: PropTypes.bool.isRequired,
+  isUpdating: PropTypes.bool.isRequired,
 };
 
 export default ContactListItem;
